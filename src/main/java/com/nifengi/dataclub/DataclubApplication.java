4e11c1e5cd33c5beef57b6329a4a10b63a7e09a3
yu <1254898873@qq.com>
2019-08-04 00:45:38 +0800

    create project

---
+++ src/main/java/com/nifengi/dataclub/DataclubApplication.java
@@ -0,0 +1,13 @@
+package com.nifengi.dataclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataclubApplication.class, args);
+    }
+
+}
